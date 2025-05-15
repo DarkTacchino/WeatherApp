e2ed37013d3d2b73d05918505257e440973c81c8
Arsene Lucian Manuel <st10517@iismarconipieralisi.it>
2025-05-15 09:14:00 +0200

    Refactoring MainActivity e conversione da Kotlin a Java

--- app/src/main/java/com/example/scout_android/MainActivity.kt
+++ app/src/main/java/com/example/scout_android/MainActivity.kt
@@ -1,20 +1,31 @@
-package com.example.scout_android
+package com.example.weatherapp
 
+import android.R
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var button: Button? = null
+    var listarichieste: ListView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        this.enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(
+            findViewById<View>(R.id.main)
+        ) { v: View, insets: WindowInsetsCompat ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        button = findViewById<Button>(R.id.b_invia)
+        listarichieste = findViewById<ListView>(R.id.listView)
     }
 }
